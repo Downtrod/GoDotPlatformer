a8242f8441636848acb1dbf5ae709d43195ac845
Downtrod <100236948+Downtrod@users.noreply.github.com>
2022-08-23 13:25:53 +1200

    godotgame

--- Player.gd
+++ Player.gd
@@ -17,7 +17,9 @@ onready var player_state = state.IDLE
 func _ready():
 	$AnimationPlayer.play("idle")
 	
-	
+func update_animation(anim):
+	pass
+
 func handle_state(state):
 	print(state)
 	pass
@@ -33,9 +35,9 @@ func _physics_process(delta):
 	get_input()
 	if velocity == Vector2.ZERO:
 		player_state = state.IDLE
-	elif Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		player_state = state.STARTJUMP
-	elif velocity.x != 0:
+	if velocity.x != 0:
 		player_state = state.RUNNING
 	
 	if not is_on_floor():
@@ -45,7 +47,7 @@ func _physics_process(delta):
 			player_state = state.FALL
 	
 	handle_state(player_state)
-	update_animation(player_state)
+	update_animation(player_state)	
 	#set gravity
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
